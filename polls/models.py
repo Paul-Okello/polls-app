914c0893bc01e810e2edab32ce64cacd5a51324c
Paul Okello <paulokello273@gmail.com>
2020-10-14 16:16:53 +0300

    create models for Question and Choice

--- polls/models.py
+++ polls/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_next =  models.CharField(max_length=20)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.name = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
