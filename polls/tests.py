50c67b8cb1019e4f4b42efcf022bda70676cf20b
Paul Okello <paulokello273@gmail.com>
2020-10-15 13:08:40 +0300

    debugging tests

--- polls/tests.py
+++ polls/tests.py
@@ -22,10 +22,11 @@ class QuestionModelsTests(TestCase):
         self.assertIs(old_question.was_published_recently(), False)
 
     def test_was_published_recently_with_recent_question(self):
+
         """
-    was_published_recently() returns True for questions whose pub_date
-    is within the last day.
-    """
-    time = timezone.now() - datetime.timedelta(hours=23, minutes=59)
-    recent_question = Question(pub_date=time)
-    self.assertIs(recent_question.was_published_recently(), True)
+            was_published_recently() returns True for questions whose pub_date
+            is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
