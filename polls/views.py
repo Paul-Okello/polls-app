eda56c6feee17d306bb1efdecc7f21031eac0cb5
Paul Okello <paulokello273@gmail.com>
2020-10-15 13:18:24 +0300

    hide questions published in the future

--- polls/views.py
+++ polls/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import get_object_or_404, render
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -14,8 +15,8 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        #returns the last five published questions
-        return Question.objects.order_by('-pub_date')[:5]
+        #returns the last five published questions(not including those published in the future)
+        return Question.objects.filter(pub_date__lte = timezone.now()).order_by('-pub_date')[:5]
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
